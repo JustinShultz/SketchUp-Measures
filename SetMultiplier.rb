c0d34bb22f5c2ef2a98942a264f305549767cab0
Justin Shultz <jss5237@gmail.com>
2019-10-14 16:06:25 -0400

    Initial commit

--- SetMultiplier.rb
+++ SetMultiplier.rb
@@ -1,5 +1,5 @@
 ######################################################################
-#  Copyright (c) 2008-2016, Alliance for Sustainable Energy.  
+#  Written by Justin Shultz.  
 #  All rights reserved.
 #  
 #  This library is free software; you can redistribute it and/or
